3a7bc69f01358eec4dc777d9913190cc3813e457
fartun <fartunwario34@gmail.com>
2023-06-28 21:44:18 +0300

    completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,18 @@
 class ItemsController < ApplicationController
-end
+    def index
+        items = [
+          {
+            id: 1,
+            name: 'Non-stick pan',
+            description: 'Sticks a bit',
+            price: 10,
+            user: {
+              id: 1,
+              username: 'Dwayne',
+              city: 'Los Angeles'
+            }
+          }
+        ]
+        render json: items
+      end
+    end
